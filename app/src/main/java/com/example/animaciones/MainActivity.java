7bb69ad3849f7922155de214013931e943e63355
Leandro <leo250703@gmail.com>
2022-05-19 12:58:56 -0400

    Initial commit

---
+++ app/src/main/java/com/example/animaciones/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.animaciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    //Atributos -> views
+
+    private Button btnMover;
+    private ImageView ivImagen;
+
+    private Animation leftRight;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inicializarVistas();
+        inicializarAnimaciones();
+        btnMover.setOnClickListener(view -> {
+            leftRight.setDuration(5000);
+            ivImagen.setAnimation(leftRight);
+        });
+    }
+
+    private void inicializarAnimaciones() {
+        leftRight = AnimationUtils.loadAnimation(this,R.anim.left_right);
+    }
+
+    private void inicializarVistas() {
+        btnMover = findViewById(R.id.btnMover);
+        ivImagen = findViewById(R.id.ivImagen);
+    }
+}
