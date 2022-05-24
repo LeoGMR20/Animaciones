532c759a8a0d6758c5a35795d601283cc734c5bb
LeoGMR20 <leo250703@gmail.com>
2022-05-23 20:20:32 -0400

    Commit 23/05/2022 20:19 (Leandro Morales) -> Set de las animaciones de movimiento, transparencia y escalado

--- app/src/main/java/com/example/animaciones/MainActivity.java
+++ app/src/main/java/com/example/animaciones/MainActivity.java
@@ -14,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
     private Button btnMover;
     private ImageView ivImagen;
 
-    private Animation leftRight;
+    //Animación
+    private Animation leftRight, escalado, transparencia;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,13 +24,38 @@ public class MainActivity extends AppCompatActivity {
         inicializarVistas();
         inicializarAnimaciones();
         btnMover.setOnClickListener(view -> {
-            leftRight.setDuration(5000);
-            ivImagen.setAnimation(leftRight);
+            ejecutarAnimacionTransparencia();
         });
+    }
+
+    private void ejecutarAnimacionMovimiento() {
+        //La duración de la animación
+        //ese tiempo se mide en milisegundos...
+        leftRight.setDuration(5000);
+        //La vista en cuestión debe arrancar la animación
+        ivImagen.startAnimation(leftRight);
+    }
+
+    private void ejecutarAnimacionEscalado(){
+        escalado.setDuration(5000);
+        //A parte de us duración, se puede configurar también
+        //1) un modo de reptición -> repito la animación tal cuel de tamaño real a más
+        //                        -> efecto inverso, aplico la animación que has
+        //configurar el nýmero de repeticieones si quierne que la animación se repita más de una vez
+        escalado.setRepeatMode(Animation.REVERSE);
+        escalado.setRepeatCount(Animation.INFINITE);
+        ivImagen.startAnimation(escalado);
+    }
+
+    private void ejecutarAnimacionTransparencia(){
+        transparencia.setDuration(7000);
+        ivImagen.startAnimation(transparencia);
     }
 
     private void inicializarAnimaciones() {
         leftRight = AnimationUtils.loadAnimation(this,R.anim.left_right);
+        escalado = AnimationUtils.loadAnimation(this,R.anim.escalado);
+        transparencia = AnimationUtils.loadAnimation(this,R.anim.transparencia);
     }
 
     private void inicializarVistas() {
